4b9f074c2e68bbbad3052af3ae06b89778104953
deathbybandaid <sam@deathbybandaid.net>
2019-03-29 08:41:38 -0400

    import issue

--- sopel_modules/rpg/rpg.py
+++ sopel_modules/rpg/rpg.py
@@ -21,9 +21,9 @@ Triggers for usage
 
 
 # Base command
-@sopel.module.commands('rpg')
-@sopel.module.thread(True)
+@module.commands('rpg')
 def rpg_trigger_main(bot, trigger):
+    bot.say("here")
     command_type = 'normalcom'
     triggerargsarray = spicemanip.main(trigger.group(2), 'create')
     execute_start(bot, trigger, triggerargsarray, command_type)
