858c3ea091dfc6b9cf24e6264c8814d9ca2fd389
deathbybandaid <sam@deathbybandaid.net>
2019-03-29 15:30:49 -0400

    implement osd

--- sopel_modules/rpg/rpg.py
+++ sopel_modules/rpg/rpg.py
@@ -149,9 +149,9 @@ def messagelog_error(bot, log_id, error_id):
     bot.memory['rpg']['message_display'][log_id] = newloglist
 
 
-def messagelog(bot, log_id, recipient, message):
+def messagelog(bot, log_id, recipients, message):
 
-    messagedict = {"type": "normal", "message": message, "recipient": recipient}
+    messagedict = {"type": "normal", "message": message, "recipients": recipients}
 
     bot.memory['rpg']['message_display'][log_id].append(messagedict)
 
@@ -168,15 +168,159 @@ def messagelog_exit(bot, rpg, log_id):
         else:
             message = messagedict["message"]
 
-        if not isinstance(message, list):
-            message = [message]
-        for entry in message:
-            if messagedict["type"] == 'error':
-                bot.notice(entry, rpg.instigator)
-            else:
-                bot.say(entry, messagedict["recipient"])
+        if messagedict["type"] == 'error':
+            osd(bot, rpg.instigator, 'notice', message)
+        else:
+            osd(bot, messagedict["recipients"], 'say', message)
 
     del bot.memory['rpg']['message_display'][log_id]
+
+
+"""
+On Screen Text
+"""
+
+
+def osd(bot, target_array, text_type_array, text_array):
+
+    # if text_array is a string, make it an array
+    textarraycomplete = []
+    if not isinstance(text_array, list):
+        textarraycomplete.append(text_array)
+    else:
+        for x in text_array:
+            textarraycomplete.append(x)
+
+    # if target_array is a string, make it an array
+    texttargetarray = []
+    if not isinstance(target_array, list):
+        if not str(target_array).startswith("#"):
+            target_array = nick_actual(bot, str(target_array))
+        texttargetarray.append(target_array)
+    else:
+        for target in target_array:
+            if not str(target).startswith("#"):
+                target = nick_actual(bot, str(target))
+            texttargetarray.append(target)
+
+    # Handling for text_type
+    texttypearray = []
+    if not isinstance(text_type_array, list):
+        for i in range(len(texttargetarray)):
+            texttypearray.append(str(text_type_array))
+    else:
+        for x in text_type_array:
+            texttypearray.append(str(x))
+    text_array_common = max(((item, texttypearray.count(item)) for item in set(texttypearray)), key=lambda a: a[1])[0]
+
+    # make sure len() equals
+    if len(texttargetarray) > len(texttypearray):
+        while len(texttargetarray) > len(texttypearray):
+            texttypearray.append(text_array_common)
+    elif len(texttargetarray) < len(texttypearray):
+        while len(texttargetarray) < len(texttypearray):
+            texttargetarray.append('osd_error_handle')
+
+    # Rebuild the text array to ensure string lengths
+
+    for target, text_type in zip(texttargetarray, texttypearray):
+
+        if target == 'osd_error_handle':
+            dont_say_it = 1
+        else:
+
+            # Text array
+            temptextarray = []
+
+            # Notice handling
+            if text_type == 'notice':
+                temptextarray.insert(0, target + ", ")
+                # temptextarray.append(target + ", ")
+            for part in textarraycomplete:
+                temptextarray.append(part)
+
+            # 'say' can equal 'priv'
+            if text_type == 'say' and not str(target).startswith("#"):
+                text_type = 'priv'
+
+            # Make sure no individual string ins longer than it needs to be
+            currentstring = ''
+            texttargetarray = []
+            for textstring in temptextarray:
+                if len(textstring) > osd_limit:
+                    chunks = textstring.split()
+                    for chunk in chunks:
+                        if currentstring == '':
+                            currentstring = chunk
+                        else:
+                            tempstring = str(currentstring + " " + chunk)
+                            if len(tempstring) <= osd_limit:
+                                currentstring = tempstring
+                            else:
+                                texttargetarray.append(currentstring)
+                                currentstring = chunk
+                    if currentstring != '':
+                        texttargetarray.append(currentstring)
+                else:
+                    texttargetarray.append(textstring)
+
+            # Split text to display nicely
+            combinedtextarray = []
+            currentstring = ''
+            for textstring in texttargetarray:
+                if currentstring == '':
+                    currentstring = textstring
+                elif len(textstring) > osd_limit:
+                    if currentstring != '':
+                        combinedtextarray.append(currentstring)
+                        currentstring = ''
+                    combinedtextarray.append(textstring)
+                else:
+                    tempstring = currentstring + "   " + textstring
+                    if len(tempstring) <= osd_limit:
+                        currentstring = tempstring
+                    else:
+                        combinedtextarray.append(currentstring)
+                        currentstring = textstring
+            if currentstring != '':
+                combinedtextarray.append(currentstring)
+
+            # display
+            textparts = len(combinedtextarray)
+            textpartsleft = textparts
+            for combinedline in combinedtextarray:
+                if text_type == 'action' and textparts == textpartsleft:
+                    bot.action(combinedline, target)
+                elif str(target).startswith("#"):
+                    bot.msg(target, combinedline)
+                elif text_type == 'notice' or text_type == 'priv':
+                    bot.notice(combinedline, target)
+                elif text_type == 'say':
+                    bot.say(combinedline)
+                else:
+                    bot.say(combinedline)
+                textpartsleft = textpartsleft - 1
+
+
+"""
+How to Display Nicks
+"""
+
+
+# Outputs Nicks with correct capitalization
+def nick_actual(bot, nick, userlist=[]):
+    nick_actual = nick
+    if userlist != []:
+        for u in userlist:
+            if u.lower() == nick_actual.lower():
+                nick_actual = u
+                continue
+        return nick_actual
+    for u in bot.users:
+        if u.lower() == nick_actual.lower():
+            nick_actual = u
+            continue
+    return nick_actual
 
 
 """
