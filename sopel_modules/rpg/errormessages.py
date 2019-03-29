20a57d781b1a7a2b498aadbc6f3e8bd19c99bd7c
deathbybandaid <sam@deathbybandaid.net>
2019-03-29 14:43:29 -0400

    test

--- sopel_modules/rpg/errormessages.py
+++ sopel_modules/rpg/errormessages.py
@@ -4,4 +4,6 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 
 # pylama:ignore=W,E201,E202,E203,E221,E222,w292,E231
 
-error_message_dict = {}
+error_message_dict = {
+                        "privmsg_no": "RPG must be run in channel.",
+                        }
