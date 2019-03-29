12f38328133de5ebbc07fe9dfff1cf6782c55307
deathbybandaid <sam@deathbybandaid.net>
2019-03-29 10:03:55 -0400

    testing an update from IRC

--- sopel_modules/rpg/rpg.py
+++ sopel_modules/rpg/rpg.py
@@ -27,6 +27,7 @@ Triggers for usage
 def rpg_trigger_normal(bot, trigger):
     command_type = str(current_function()).split("rpg_trigger_")[-1]
     bot.say(command_type)
+    bot.say("testing update command")
     triggerargsarray = spicemanip.main(trigger.group(2), 'create')
     execute_start(bot, trigger, triggerargsarray, command_type)
 
