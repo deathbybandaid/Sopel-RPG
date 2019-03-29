7830c733db3510eee9b139aa67b05b5c25441288
deathbybandaid <sam@deathbybandaid.net>
2019-03-29 10:29:17 -0400

    testing multiple command types

--- sopel_modules/rpg/rpg.py
+++ sopel_modules/rpg/rpg.py
@@ -28,6 +28,21 @@ def rpg_trigger_normal(bot, trigger):
     execute_start(bot, trigger, str(current_function()).split("rpg_trigger_")[-1])
 
 
+# work with /me ACTION
+@module.rule('^(?:challenges|(?:fi(?:ght|te)|duel)s(?:\s+with)?)\s+([a-zA-Z0-9\[\]\\`_\^\{\|\}-]{1,32}).*')
+@module.intent('ACTION')
+@sopel.module.thread(True)
+def rpg_trigger_action(bot, trigger):
+    execute_start(bot, trigger, str(current_function()).split("rpg_trigger_")[-1])
+
+
+# bot.nick do this
+@nickname_commands('rpg')
+@sopel.module.thread(True)
+def rpg_trigger_nick_command(bot, trigger):
+    execute_start(bot, trigger, str(current_function()).split("rpg_trigger_")[-1])
+
+
 """
 Command Processing
 """
@@ -35,7 +50,7 @@ Command Processing
 
 def execute_start(bot, trigger, command_type):
 
-    bot.say("loading a test of rpg")
+    bot.say("Testing RPG    command_type=" + command_type)
 
     # triggerargsarray = spicemanip.main(trigger.group(2), 'create')
 
